350803d7c7451d0322358b84b5a215fb352405fa
legofernando1999 <82280950+legofernando1999@users.noreply.github.com>
2022-09-22 10:17:12 -0500

    Update main.py
    Removed the file uploader, as it is now in the visualizer

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-from distutils.command.upload import upload
 import streamlit as st
-import numpy as np
-import pandas as pd
-import json
 from config import DATA_DIR
-from fileConverter import xlsx_to_json, get_data_from_json
 
 st.set_page_config(
     page_title="Hello",
@@ -28,21 +23,3 @@ st.markdown(
     - To get in touch [click here](https://ennova.us/contact/)
     """
     )
-
-# File uploader
-uploaded_file = st.sidebar.file_uploader('Upload fluid file:', type = ('xlsx', 'json'))
-
-if uploaded_file is not None:
-    if uploaded_file.type == 'application/json':
-        st.success("json file successfully uploaded")
-        fluid = uploaded_file.name.replace('.json', '')
-        prop_json = uploaded_file
-    elif uploaded_file.type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-        st.success("xlsx file successfully uploaded")
-        fluid = uploaded_file.name.replace('.xlsx', '')
-        prop_json = xlsx_to_json(uploaded_file)
-    
-    # properties from json
-    coord_label, coord_unit, coord_range, prop_label, prop_unit, Property = get_data_from_json(prop_json)
-else:
-    st.warning('Please upload a fluid file to begin')
