c6f436971a67eb83794569c9488ef82773c76b19
Fernando <munoz@ennova.us>
2022-09-23 18:10:22 -0500

    modified:   pages/1_Visualizer.py     "Updated UI"

--- pages/1_Visualizer.py
+++ pages/1_Visualizer.py
@@ -34,7 +34,7 @@ def series_options(axis: str) -> st.radio:
     const_coord_dict = {coord[0]: (f'constant {coord_label[1]}', f'constant {coord_label[2]}'), 
                     coord[1]: (f'constant {coord_label[0]}', f'constant {coord_label[2]}'), 
                     coord[2]: (f'constant {coord_label[0]}', f'constant {coord_label[1]}')}
-    return st.sidebar.radio('Choose the type of the series:', const_coord_dict[axis])
+    return tab3.radio('Choose the type of the series:', const_coord_dict[axis])
 
 # DataFrame converter: DF -> csv/excel/json
 # cjsisco note: should be able to output in different formats
@@ -51,13 +51,10 @@ def convert_df(df: pd.DataFrame, to_type: str):
         return output.getvalue()
 
 def interruptor(df: pd.DataFrame, fig, xaxis, yaxis, type_series):
-    with cont4:
-        switch = st.radio('Switch between plot and table:', ('Plot', 'Table'), horizontal=True)
-        if switch == 'Plot':
-            st.plotly_chart(fig)
-        else:
-            st.dataframe(df)
-        to_type = st.radio('Select the data format:', ('csv', 'xlsx', 'json'))
+    tab4.plotly_chart(fig)
+    with tab5:
+        st.dataframe(df.copy())
+        to_type = st.radio('Select data format:', ('csv', 'xlsx', 'json'))
         nombre = f'{xaxis}_vs_{yaxis}_{type_series}.{to_type}'
         st.download_button(label="Download data", data=convert_df(df, to_type), file_name=nombre)
 
@@ -68,7 +65,7 @@ def isocurve(type_series: str) -> tuple[np.array]:
         nCoord_array: array of indices of the multiple isocurves
     """
     idx = get_idx(const_coord, type_series)
-    iso = st.sidebar.multiselect(f'Choose from the available {coord[idx]}:', coord_range[idx])
+    iso = tab3.multiselect(f'Choose from the available {coord[idx]}:', coord_range[idx])
     nCoord_array = [get_idx(coord_range[idx], iso[i]) for i in range(len(iso))]
 
     iso = np.array(iso)
@@ -78,7 +75,7 @@ def isocurve(type_series: str) -> tuple[np.array]:
 def coordinates(axis: str, type_series: str) -> tuple[str]:
     """
     Gets value of single coordinate (number input at bottom of sidebar)
-    
+
     returns:
         coord_value: value of coordinate 
         titulo: plot title
@@ -88,7 +85,7 @@ def coordinates(axis: str, type_series: str) -> tuple[str]:
     idx = np.delete(np.array([0, 1, 2]), [axis_idx, type_idx])
     minimo=coord_range[idx][0][0]
     maximo=coord_range[idx][0][-1]
-    coord_value = st.sidebar.number_input(f'Enter a {coord[idx][0]}:', min_value=minimo, max_value=maximo)
+    coord_value = tab3.number_input(f'Enter a {coord[idx][0]}:', min_value=minimo, max_value=maximo)
 
     a = (f'{coord_label[0]}: {str(coord_value)} {coord_unit[0]}', 
         f'{coord_label[1]}: {str(coord_value)} {coord_unit[1]}', 
@@ -139,9 +136,8 @@ def coord_on_axis(xaxis: str, yaxis: str, type_series: str):
         axis = yaxis
         nprop = get_idx(prop, xaxis)
     
-    with cont3:
-        iso, nCoord_array = isocurve(type_series)
-        coord_value, titulo = coordinates(axis, type_series)
+    iso, nCoord_array = isocurve(type_series)
+    coord_value, titulo = coordinates(axis, type_series)
 
     idx = get_idx(coord, axis)
     idx2 = get_idx(const_coord, type_series)
@@ -167,10 +163,8 @@ def prop_vs_prop(xaxis: str, yaxis: str, type_series: str, Not_fixed: str):
     returns:
         plot, table, file
     """
-    with cont3:
-        iso, nCoord_array = isocurve(type_series)
-        # nCoord_idx, titulo = coordinates(Not_fixed, type_series)
-        coord_value, titulo = coordinates(Not_fixed, type_series)
+    iso, nCoord_array = isocurve(type_series)
+    coord_value, titulo = coordinates(Not_fixed, type_series)
 
     nprop1 = get_idx(prop, xaxis)
     nprop2 = get_idx(prop, yaxis)
@@ -200,76 +194,65 @@ def prop_vs_prop(xaxis: str, yaxis: str, type_series: str, Not_fixed: str):
 
 
 #%%
-# containers to separate content and allow dynamic selection
+# containers to structure page
 maincont = st.container()
 cont1 = st.container()
 cont2 = st.container()
 cont3 = st.container()
-cont4 = st.container()
+tab1, tab2, tab3 = st.sidebar.tabs(["File uploader", "Choose axes", "Series type"])
 
 # set file path 
 # ruta = DATA_DIR / 'output' / 'S14-SAFT-MILA2020.json'
 
 # File uploader
-uploaded_file = st.sidebar.file_uploader('Upload fluid file:', type = ('xlsx', 'json'))
+uploaded_file = tab1.file_uploader('Upload fluid file:', type = ('xlsx', 'json'))
 
 if uploaded_file is not None:
+    if uploaded_file.type == 'application/json':
+        fluid = uploaded_file.name.replace('.json', '')
+        fluid = fluid.replace('lookup_', '')
+        prop_json = uploaded_file
+    elif uploaded_file.type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+        fluid, prop_json = fc.xlsx_to_json(uploaded_file)
+    
+    # properties from json
+    coord_label, coord_unit, coord_range, prop_label, prop_unit, prop_table = fc.get_data_from_json(prop_json)
+
+    # some useful arrays
+    const_coord = np.array([f'constant {coord_label[0]}', f'constant {coord_label[1]}', f'constant {coord_label[2]}'])
+    coord = [f"{coord_label[i]} [{coord_unit[i]}]" for i in range(len(coord_label))]    
+    prop = [f"{prop_label[i]} [{prop_unit[i]}]" for i in range(len(prop_label))]        
+    variables = np.array(coord + prop)
+    coord = np.array(coord)     #coordinate labels + units
+    prop = np.array(prop)       #property labels + units
+    prop_range = np.linspace(0, len(prop_label) - 1, len(prop_label))
+
+    maincont.header(fluid)
     with maincont:
-        if uploaded_file.type == 'application/json':
-            # st.success("json file successfully uploaded")
-            fluid = uploaded_file.name.replace('.json', '')
-            fluid = fluid.replace('lookup_', '')
-            prop_json = uploaded_file
-        elif uploaded_file.type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-            # st.success("xlsx file successfully uploaded")
-            fluid, prop_json = fc.xlsx_to_json(uploaded_file)
-        
-        # properties from json
-        coord_label, coord_unit, coord_range, prop_label, prop_unit, prop_table = fc.get_data_from_json(prop_json)
+        tab4, tab5 = st.tabs(["Plot", "Table"])
 
-        # some useful arrays
-        const_coord = np.array([f'constant {coord_label[0]}', f'constant {coord_label[1]}', f'constant {coord_label[2]}'])
-        coord = [f"{coord_label[i]} [{coord_unit[i]}]" for i in range(len(coord_label))]    
-        prop = [f"{prop_label[i]} [{prop_unit[i]}]" for i in range(len(prop_label))]        
-        variables = np.array(coord + prop)
-        coord = np.array(coord)     #coordinate labels + units
-        prop = np.array(prop)       #property labels + units
-        prop_range = np.linspace(0, len(prop_label) - 1, len(prop_label))
+    # Sidebar filters
+    with cont1:
+        tab2.header('Choose axes:')
+        xaxis = tab2.selectbox('Select x-axis:', variables)
+    
+    # choose what to display on main page from user selection
+    if xaxis in coord:
+        yaxis = tab2.selectbox('Select y-axis:', prop)    
+        type_series = series_options(xaxis)
+        coord_on_axis(xaxis, yaxis, type_series)
+    else:           #xaxis == Property
+        yaxis = tab2.selectbox('Select y-axis:', variables)
 
-        st.write(f"# {fluid}")
+        if yaxis in coord: 
+            type_series = series_options(yaxis)
+            coord_on_axis(xaxis, yaxis, type_series) 
+        else:       #yaxis == Property 
+            Not_fixed = tab2.selectbox('Which parameter should vary?', coord)
+            type_series = series_options(Not_fixed)
+            prop_vs_prop(xaxis, yaxis, type_series, Not_fixed) 
 
-        # Sidebar filters
-        st.sidebar.header('Choose axes:')
-        xaxis = st.sidebar.selectbox('Select x-axis:', variables)
-        
-        # choose what to display on main page from user selection
-        if xaxis in coord:
-            with cont1:
-                yaxis = st.sidebar.selectbox('Select y-axis:', prop)
-                
-            with cont2:    
-                type_series = series_options(xaxis)
-
-            coord_on_axis(xaxis, yaxis, type_series)
-        else:           #xaxis == Property
-            with cont1:
-                yaxis = st.sidebar.selectbox('Select y-axis:', variables)
-
-            if yaxis in coord: 
-                with cont2:
-                    type_series = series_options(yaxis)
-
-                coord_on_axis(xaxis, yaxis, type_series) 
-            else:       #yaxis == Property 
-                with cont1:
-                    Not_fixed = st.sidebar.selectbox('Which parameter should vary?', coord)
-
-                with cont2:
-                    type_series = series_options(Not_fixed)
-                    
-                prop_vs_prop(xaxis, yaxis, type_series, Not_fixed) 
-
-        st.sidebar.markdown("#")
+    st.sidebar.markdown("#")
 else:
-    with maincont:
-        st.warning('Please upload a fluid file to begin')
+    st.warning('Please upload a fluid file to begin')
+# %%
